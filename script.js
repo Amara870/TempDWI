a2641a49bb12c635ff16b697bf6c033223b7d16d
Amara Hill <ahill87105@uat.edu>
2026-06-22 02:05:50 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+// save info function
 function saveInfo() {
+    // add element ids to variable
     let txtUsername = document.getElementById("txtUsername");
     let txtSkill = document.getElementById("txtSkill");
     let txtWeapon = document.getElementById("txtWeapon");
     let numHealth = document.getElementById("numHealth");
     let numPoints = document.getElementById("numPoints");
 
+    // format user inputs
     let output = "Username: " + txtUsername.value + "\n\n" +
         "Special Skill: " + txtSkill.value + "\n\n" +
         "Weapon: " + txtWeapon.value + "\n\n" +
         "Health: " + numHealth.value + "\n\n" +
         "Total Points: " + numPoints.value;
+    // display inputs
     document.getElementById("textHero").value = output;
 
+
+    // reset input boxes
     txtUsername.value = "";
     txtSkill.value = "";
     numHealth.value = "";
